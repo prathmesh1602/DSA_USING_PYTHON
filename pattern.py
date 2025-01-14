8d9fe8aaf031e4edbcbf81168345440a786e17df
prathmesh16 <prathmeshshinde4516@gmail.com>
2025-01-14 14:08:42 +0530

    pattern Questions

--- pattern.py
+++ pattern.py
@@ -1,9 +1,34 @@
+# def Pattern(n):
+#     for i in range(n+1):
+#         for j in range(i):
+#             print("*",end=" ")
+#         print()    
+# Pattern(10)        
+
+# def Pattern(n):
+#     for i in range(n+1):
+#         for j in range(1,i+1):
+#             print(i,end=" ")
+#         print()    
+# Pattern(10)        
+       
+# def Pattern(n):
+#     for i in range(n+1):
+#         for j in range(1,n-i):
+#             print("*",end=" ")
+#         print()    
+# Pattern(10)  
+
+# def Pattern(n):
+#     for i in range(n+1):
+#         for j in range(1,(n+1)-i):
+#             print(j,end=" ")
+#         print()    
+# Pattern(10) 
+
 def Pattern(n):
     for i in range(n+1):
-        for j in range(n+1):
-            print("*",end=" ")
+        for j in range(1,(n+1)-i):
+            print(j,end=" ")
         print()    
-Pattern(10)        
-
-        
-
+Pattern(10) 
