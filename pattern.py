9246b6aaca7db193aa26d0063c7fae40ce74b7e1
prathmesh16 <prathmeshshinde4516@gmail.com>
2025-01-15 21:24:15 +0530

    15-01-2025

--- pattern.py
+++ pattern.py
@@ -26,9 +26,49 @@
 #         print()    
 # Pattern(10) 
 
+# def Pattern(n):
+#     for i in range(n+1):
+#         for j in range(1,(n+1)-i):
+#             print(j,end=" ")
+#         print()    
+# Pattern(10) 
+# def Pattern(n):
+#     for i in range(n):
+         
+#         for j in range(n-i-1):
+#             print(" ",end=" ")
+#         for k in range(i*2-1):
+#             print("*",end=" ")
+#         for l in range(n-i-1):
+#             print(" ",end=" ")        
+
+#         print()    
+# Pattern(4) 
+
+
+
+# def Pattern(n):
+#     for i in range(n,0,-1):
+         
+#         for j in range(n-i):
+#             print(" ",end=" ")
+#         for k in range(i*2-1):
+#             print("*",end=" ")
+#         for l in range(n-i):
+#             print(" ",end=" ")        
+
+#         print()    
+# Pattern(4) 
+
 def Pattern(n):
-    for i in range(n+1):
-        for j in range(1,(n+1)-i):
-            print(j,end=" ")
+    for i in range(n,0,-1):
+         
+        for j in range(n-i):
+            print(" ",end=" ")
+        for k in range(i*2-1):
+            print("*",end=" ")
+        for l in range(n-i):
+            print(" ",end=" ")        
+
         print()    
-Pattern(10) 
+Pattern(4) 
